769dfe2d4904787ec4463888c87040b8a573cd63
Ayushka Partohap <partohap.ayushka@gmail.com>
2019-06-04 00:41:29 +0700

    Cat-clicker: requirements-2

--- cat-clicker/main.js
+++ cat-clicker/main.js
@@ -1,12 +1,20 @@
+const CAT_NAMES = ["Agus", "Budi"]
 document.addEventListener("DOMContentLoaded", function() {
-  const catElm = document.querySelector("#cat")
-  const clicksElm = document.querySelector("#clicks")
-  const state = { clicks: 0 }
+  const catElms = document.querySelectorAll(".cat")
+  const state = {
+    cats: [{ name: CAT_NAMES[0], clicks: 0 }, { name: CAT_NAMES[1], clicks: 0 }]
+  }
 
-  clicksElm.innerHTML = state.clicks
+  catElms.forEach((cat, index) => {
+    const nameElm = cat.querySelector(".cat-name")
+    const clickElm = cat.querySelector(".clicks")
+    const catState = state.cats[index]
+    nameElm.innerHTML = catState.name
+    clickElm.innerHTML = catState.clicks
 
-  catElm.addEventListener("click", function() {
-    state.clicks++
-    clicksElm.innerHTML = state.clicks
+    cat.addEventListener("click", function() {
+      catState.clicks++
+      clickElm.innerHTML = catState.clicks
+    })
   })
 })
